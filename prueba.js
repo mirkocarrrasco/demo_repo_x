cd412fbd526ac2d3a7f151b96c827aa720b00274
Mirko <gaoshark_27@hotmail.com>
2021-03-13 11:20:30 -0500

    División

--- prueba.js
+++ prueba.js
@@ -10,3 +10,6 @@ function myFunctionMultiplicacion(p1, p2) {
   return p1 * p2;   // The function returns the product of p1 and p2
 }
 
+function myFunctionDivision(p1, p2) {
+  return p1 / p2;   // The function returns the product of p1 and p2
+}
